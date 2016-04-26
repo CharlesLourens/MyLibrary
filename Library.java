fdf9f5ef5ad63e00a6c31b76397f889d7dfd8029
Charles Lourens <github@lourensonline.net>
2016-04-26 16:07:23 -0400

    :i continueing my work :wq

--- Library.java
+++ Library.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 
 class Library{
 	List<LibraryBook> books;
@@ -14,27 +15,24 @@ class Library{
 
 	public static void main(String[] Args){
 		Library library = new Library();
-		library.initialize(library);
+		library.initialize();
 		while (library.showInterface()){
 			// This is where magic happens...
 		}
 	}
-	private void initialize(Library library){
-		library.books = new ArrayList<LibraryBook>();
-		library.addBook(books, "Lord of the Rings", "JRR Tolkin");
-		library.addBook(books, "Room", "Emma Donoghue");
-		library.addBook(books, "Being Ernest", "Oscar Wilde");
+	private void initialize(){
+		this.books = new ArrayList<LibraryBook>();
+		this.addBook(books, "Lord of the Rings", "JRR Tolkin");
+		this.addBook(books, "Room", "Emma Donoghue");
+		this.addBook(books, "Being Ernest", "Oscar Wilde");
 
-		library.users = new ArrayList<LibrarySystemUser>();
-		library.addUser(users, "admin", "admin", "Library", "Admin", true);
+		this.users = new ArrayList<LibrarySystemUser>();
+		this.addUser(users, "admin", "admin", "Library", "Admin", true);
 
 	}
 
 	private boolean showInterface(){
-		boolean activeUser = false;
-		while (activeUser){
-			
-		}
+		LibrarySystemUser activeUser = null;
 		System.out.println("Select your Action");	
 		System.out.println("1-Login");
 		System.out.println("9-Exit");
@@ -42,7 +40,7 @@ class Library{
 		try {
 			String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
 			if (s.toUpperCase().equals("LOGIN") || s.substring(0).equals("1")){
-//				login();
+				activeUser = userLogin(users);
 			} else if (s.toUpperCase().equals("EXIT") || s.substring(0).equals("9")){
 				return false;
 			}
@@ -50,7 +48,75 @@ class Library{
 			System.out.println("IOE thrown");
 			return false;
 		}
+
+		while (activeUser != null) {
+			System.out.println("Welcome "+activeUser.getFirstName()+" "+activeUser.getLastName());
+			System.out.println("What do you want to do today?");
+			System.out.println("2-Take out a Book");
+			System.out.println("3-Return a book");
+			if (activeUser.getIsEmployee()){
+				System.out.println("4-Add a Book");
+				System.out.println("5-Remove a Book");
+				System.out.println("6-Add a Library User");
+				System.out.println("7-Remove a Library User");
+			}
+			System.out.println("8-Logout");
+			try {
+				String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
+				if (s.toUpperCase().equals("TAKE OUT A BOOK") || s.substring(0).equals("2")){
+//					takeOutBook(activeUser);
+				} else if (s.toUpperCase().equals("RETURN A BOOK") || s.substring(0).equals("3")){
+//					returnBook(activeUser);
+				} else if (s.toUpperCase().equals("ADD A BOOK") || s.substring(0).equals("4")){
+//					addBook(this.books);
+				} else if (s.toUpperCase().equals("REMOVE A BOOK") || s.substring(0).equals("5")){
+//					removeBook(this.books);
+				} else if (s.toUpperCase().equals("ADD A LIBRARY USER") || s.substring(0).equals("6")){
+//					addUser(this.users);
+				} else if (s.toUpperCase().equals("REMOVE A LIBRARY USER") || s.substring(0).equals("7")){
+//					removeUser(this.users);
+				} else if (s.toUpperCase().equals("LOGOUT") || s.substring(0).equals("8")){
+					activeUser = null;
+				} else {
+					System.out.println("Not a valid option try using only the option number");
+				}				
+			} catch (IOException ioe){
+				System.out.println("IOE thrown");
+				return false;
+			}
+		}
 		return true;
+	}
+	
+	private LibrarySystemUser userLogin(List<LibrarySystemUser> validUsers) {
+		System.out.println("Enter UserName and hit enter to login, or leave blank and hit enter to return to main menu");
+		try {
+			String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
+			if (s.length() > 0){
+				Iterator<LibrarySystemUser> i = validUsers.iterator();
+				while (i.hasNext()){
+					LibrarySystemUser thisUser = i.next();
+					if (thisUser.getUserName().toUpperCase().equals(s.toUpperCase())) {
+						System.out.println("Enter Password and hit enter");
+						s = new BufferedReader(new InputStreamReader(System.in)).readLine();
+						if (thisUser.getPassword().equals(s)){
+							return thisUser;
+						} else {
+							System.out.println("Invalid Credentials");
+							return null;
+						}
+					}
+				}
+				System.out.println("UserName not found");
+				return null;
+			} else {
+				return null;
+			}
+			
+		} catch (IOException ioe){
+			System.out.println("IOE thrown");
+		}
+		return null;
 	}
 	
 
