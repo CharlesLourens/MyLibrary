3dbf09ae6d4a0440685f2a73ad34fe1bb71b046c
Charles Lourens <github@lourensonline.net>
2016-04-27 15:47:13 -0400

    All functionality is there
    :w
    :q
    
    :q
    :w
    :x
    
    ZZ
    :q!
    
    All the functionality is there

--- Library.java
+++ Library.java
@@ -22,12 +22,12 @@ class Library{
 	}
 	private void initialize(){
 		this.books = new ArrayList<LibraryBook>();
-		this.addBook(books, "Lord of the Rings", "JRR Tolkin");
-		this.addBook(books, "Room", "Emma Donoghue");
-		this.addBook(books, "Being Ernest", "Oscar Wilde");
+		this.addBook("Lord of the Rings", "JRR Tolkin");
+		this.addBook("Room", "Emma Donoghue");
+		this.addBook("Being Ernest", "Oscar Wilde");
 
 		this.users = new ArrayList<LibrarySystemUser>();
-		this.addUser(users, "admin", "admin", "Library", "Admin", true);
+		this.addUser("admin", "admin", "Library", "Admin", true);
 
 	}
 
@@ -36,7 +36,6 @@ class Library{
 		System.out.println("Select your Action");	
 		System.out.println("1-Login");
 		System.out.println("9-Exit");
-		int i = 0;
 		try {
 			String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
 			if (s.toUpperCase().equals("LOGIN") || s.substring(0).equals("1")){
@@ -52,7 +51,7 @@ class Library{
 		while (activeUser != null) {
 			System.out.println("Welcome "+activeUser.getFirstName()+" "+activeUser.getLastName());
 			System.out.println("What do you want to do today?");
-			System.out.println("2-Take out a Book");
+			System.out.println("2-Loan a Book");
 			System.out.println("3-Return a book");
 			if (activeUser.getIsEmployee()){
 				System.out.println("4-Add a Book");
@@ -63,18 +62,161 @@ class Library{
 			System.out.println("8-Logout");
 			try {
 				String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
-				if (s.toUpperCase().equals("TAKE OUT A BOOK") || s.substring(0).equals("2")){
-//					takeOutBook(activeUser);
+				if (s.toUpperCase().equals("LOAN A BOOK") || s.substring(0).equals("2")){
+					System.out.println("Please enter the book number, from the list below, to loan the book");
+					for (int i = 0; i < books.size(); i++) {
+						LibraryBook book = books.get(i);
+						if (book.getIsAvailable()){
+							System.out.println(book.getBookNumber()+" - "+book.getBookName()+" ("+book.getBookAuthor()+")");							
+						}
+					}
+					try {
+						s = new BufferedReader(new InputStreamReader(System.in)).readLine();
+						if (s.length() > 0){
+							int bookNumber = Integer.parseInt(s);
+							System.out.println(activeUser.loanBook(bookNumber));
+						} 
+					} catch (IOException ioe){
+						System.out.println("IOE thrown");
+						return false;
+					} catch (NumberFormatException nfe){
+						System.out.println("That wasn't a valid book number");
+					}
 				} else if (s.toUpperCase().equals("RETURN A BOOK") || s.substring(0).equals("3")){
-//					returnBook(activeUser);
+					List<Integer> loanedBookNumbers = activeUser.getListBooksLoaned();
+					if (loanedBookNumbers.size() > 0 ){
+						System.out.println("Please enter the book number, from the list below, that you wish to return");
+						for (int i = 0; i < loanedBookNumbers.size(); i++) {
+							LibraryBook book = books.get(loanedBookNumbers.get(i));
+							System.out.println(book.getBookNumber()+" - "+book.getBookName()+" ("+book.getBookAuthor()+")");							
+						}
+						try {
+							s = new BufferedReader(new InputStreamReader(System.in)).readLine();
+							if (s.length() > 0){
+								int bookNumber = Integer.parseInt(s);
+								System.out.println(activeUser.returnBook(bookNumber));
+							} 
+						} catch (IOException ioe){
+							System.out.println("IOE thrown");
+							return false;
+						} catch (NumberFormatException nfe){
+							System.out.println("That wasn't a valid book number");
+						}					
+					} else {
+						System.out.println("You haven't got any books");
+					}
 				} else if (s.toUpperCase().equals("ADD A BOOK") || s.substring(0).equals("4")){
-//					addBook(this.books);
+					String bookName, authorName;
+					System.out.println("Please enter the book name");
+					try {
+						bookName = new BufferedReader(new InputStreamReader(System.in)).readLine();
+					} catch (IOException ioe){
+						System.out.println("IOE thrown");
+						return false;
+					}
+					System.out.println("Please enter the Author's name");
+					try {
+						authorName = new BufferedReader(new InputStreamReader(System.in)).readLine();
+					} catch (IOException ioe){
+						System.out.println("IOE thrown");
+						return false;
+					}
+					this.addBook(bookName, authorName);
 				} else if (s.toUpperCase().equals("REMOVE A BOOK") || s.substring(0).equals("5")){
-//					removeBook(this.books);
+					System.out.println("Please enter the book number, from the list below, to remove");
+					for (int i = 0; i < books.size(); i++) {
+						LibraryBook book = books.get(i);
+						if (book.getIsAvailable()){
+							System.out.println(book.getBookNumber()+" - "+book.getBookName()+" ("+book.getBookAuthor()+")");							
+						}
+					}
+					try {
+						s = new BufferedReader(new InputStreamReader(System.in)).readLine();
+						if (s.length() > 0){
+							int bookNumber = Integer.parseInt(s);
+							LibraryBook removedBook = books.get(bookNumber);
+							books.remove(removedBook);
+							System.out.println("Book removed!");
+						} 
+					} catch (IOException ioe){
+						System.out.println("IOE thrown");
+						return false;
+					} catch (NumberFormatException nfe){
+						System.out.println("That wasn't a valid book number");
+					}
 				} else if (s.toUpperCase().equals("ADD A LIBRARY USER") || s.substring(0).equals("6")){
-//					addUser(this.users);
+					String libraryUserName, passW, firstName, lastName, admin;
+					Boolean isAdmin = false;
+					System.out.println("Is this a new employee? (Y/N) Default is 'N'");
+					try {
+						admin = new BufferedReader(new InputStreamReader(System.in)).readLine();
+						if (admin.substring(0).toUpperCase().equals("Y")){
+							isAdmin = true;
+						}
+					} catch (IOException ioe){
+						System.out.println("IOE thrown");
+						return false;
+					}
+					System.out.println("Please enter the client's first name");
+					try {
+						firstName = new BufferedReader(new InputStreamReader(System.in)).readLine();
+					} catch (IOException ioe){
+						System.out.println("IOE thrown");
+						return false;
+					}
+					System.out.println("Please enter the client's last name");
+					try {
+						lastName = new BufferedReader(new InputStreamReader(System.in)).readLine();
+					} catch (IOException ioe){
+						System.out.println("IOE thrown");
+						return false;
+					}
+					System.out.println("Please enter your userName");
+					try {
+						libraryUserName = new BufferedReader(new InputStreamReader(System.in)).readLine();
+					} catch (IOException ioe){
+						System.out.println("IOE thrown");
+						return false;
+					}
+					passW = "Password";
+					Boolean validPassword = false;
+					while (!validPassword){
+						System.out.println("Please enter your password. The password must be at least 4 characters");
+						try {
+							passW = new BufferedReader(new InputStreamReader(System.in)).readLine();
+							if (passW.length() > 3){
+								validPassword = true;
+							}
+						} catch (IOException ioe){
+							System.out.println("IOE thrown");
+							return false;
+						}
+					}
+					this.addUser(libraryUserName, passW, firstName, lastName, isAdmin);
 				} else if (s.toUpperCase().equals("REMOVE A LIBRARY USER") || s.substring(0).equals("7")){
-//					removeUser(this.users);
+					System.out.println("Please enter the user number, from the list below, to remove");
+					for (int i = 0; i < users.size(); i++) {
+						LibrarySystemUser user = users.get(i);
+						System.out.println(user.getUserNumber()+" - "+user.getFirstName()+" "+user.getLastName()+" ("+user.getIsEmployee()+")");							
+					}
+					try {
+						s = new BufferedReader(new InputStreamReader(System.in)).readLine();
+						if (s.length() > 0){
+							int userNumber = Integer.parseInt(s);
+							LibrarySystemUser removedUser = users.get(userNumber);
+							if (removedUser.equals(activeUser)){
+								System.out.println("You cannot remove yourself!!");
+							} else {
+								users.remove(removedUser);
+								System.out.println("User account removed!");
+							}
+						} 
+					} catch (IOException ioe){
+						System.out.println("IOE thrown");
+						return false;
+					} catch (NumberFormatException nfe){
+						System.out.println("That wasn't a valid user number");
+					}
 				} else if (s.toUpperCase().equals("LOGOUT") || s.substring(0).equals("8")){
 					activeUser = null;
 				} else {
@@ -120,7 +262,7 @@ class Library{
 	}
 	
 
-	private void addBook(List<LibraryBook> books, String name, String author){
+	private void addBook(String name, String author){
 		int i = books.size();
 		LibraryBook book = new LibraryBook();
 		book.setBookName(name);
@@ -130,7 +272,7 @@ class Library{
 		books.add(book);
 	}
 
-	private void addUser(List<LibrarySystemUser> users, String uName, String password, String fName, String lName, boolean admin){
+	private void addUser(String uName, String password, String fName, String lName, boolean admin){
 		int i = users.size();
 		LibrarySystemUser user = new LibrarySystemUser();
 		user.setUserNumber(i++);
@@ -142,11 +284,6 @@ class Library{
 		users.add(user);
 	}
 
-	private void borrowBook(){
-		System.out.println("Available books");
-		
-	}
-	
 	class LibraryBook{
 		private int bookNumber;
 		private String bookName;
@@ -186,6 +323,7 @@ class Library{
 		private String lastName;
 		private String password;
 		private boolean isEmployee;
+		private List<Integer> booksLoaned = new ArrayList<Integer>(5);
 		
 		public int getUserNumber(){
 			return userNumber;
@@ -222,6 +360,40 @@ class Library{
 		}
 		public void setIsEmployee(boolean employee){
 			isEmployee = employee;
+		}
+		public List<Integer> getListBooksLoaned(){
+			return booksLoaned;
+		}
+		public void setListBooksLoaned(List<Integer> bookList){
+			booksLoaned = bookList;
+		}
+		public String loanBook(int bookNumber){
+			if (booksLoaned.size() > 4){
+				return "You cannot loan this book, please return one of your books first";
+			} else {
+				Iterator<LibraryBook> i = books.iterator();
+				while (i.hasNext()){
+					LibraryBook book = i.next();
+					if (book.getBookNumber() == bookNumber) {
+						booksLoaned.add(Integer.valueOf(bookNumber));
+						book.setIsAvailable(false);
+						return "Thank you, enjoy reading your book";
+					}
+				}
+				return "That selection is not available";
+			}
+		}
+		public String returnBook(int bookNumber){
+			Iterator<LibraryBook> i = books.iterator();
+			while (i.hasNext()){
+				LibraryBook book = i.next();
+				if (book.getBookNumber() == bookNumber) {
+					booksLoaned.remove(Integer.valueOf(bookNumber));
+					book.setIsAvailable(true);
+					return "Thank you for returning the book";
+				}
+			}
+			return "That selection is not available";
 		}
 	}	
 }
